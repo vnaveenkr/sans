46fd1d3bda48b57f5de46e41c0578a4d41fe1a3c
NaveenKumar Venkataraj <nvn1888@gmail.com>
2024-11-25 21:50:02 +0530

    add playall

--- संस्कृत.js
+++ संस्कृत.js
@@ -27,57 +27,91 @@ function kn2dv(str) {
     .join("");
 }
 
-function playAudioInterval(interval) {
-  console.log(interval);
-}
-
 class IntervalPlayer {
-  constructor(audioElem) {
+  constructor(audioElem, timeTable, onpause, onplay) {
     this.player = audioElem;
-    /* this.player.addEventListener("timeupdate", () => {
-      if (this.player.currentTime >= this.playEnd) {
-        this.player.pause();
-        if (this.func) this.func(this.arg);
-      }
-    }); */
+    this.timeTable = timeTable;
+    this.onpause = onpause;
+    this.onplay = onplay;
   }
+
+  findRef(time) {
+    return this.timeTable.find((x) => x.interval[0] >= time);
+  }
+
   cancelTimer() {
     if (typeof this.timer === "number") {
       clearTimeout(this.timer);
       this.timer = undefined;
     }
   }
-  onTimeout() {
-    this.player.pause();
-    if (this.func) this.func(this.arg);
-    this.cancelTimer();
-  }
-  playInterval(interval, arg, func) {
-    this.cancelTimer();
-    if (this.playStart == interval[0]) {
-      if (this.player.paused) {
-        this.player.currentTime = this.pauseTime;
-        this.player.play();
-      } else {
-        this.pauseTime = this.player.currentTime;
-        this.player.pause();
-        if (this.func) this.func(this.arg);
-      }
-      this.playStart = player.currentTime;
-      if (this.func) this.func(this.arg);
-    } else {
-      this.playStart = interval[0];
-      this.playEnd = interval[1];
-      if (this.func) this.func(this.arg);
 
-      this.arg = arg;
-      this.func = func;
-      this.timer = setTimeout(this.onTimeout, 1000*(imterval[1] - interval[0]);
-      this.player.currentTime = interval[0];
-      this.player.play();
+  onTimeout(obj) {
+    obj.cancelTimer();
+    if (obj.playall) {
+      const rec = obj.findRef(obj.player.currentTime);
+      if (obj.ref) obj.onpause(obj.ref);
+      obj.ref = rec.ref;
+      obj.playPlayer(rec);
+      obj.timer = setTimeout(
+        obj.onTimeout,
+        1000 * (rec.interval[1] - rec.interval[0]),
+        obj,
+      );
+    } else {
+      obj.pausePlayer();
+      obj.ref = null;
     }
   }
-  pause() {
+
+  playPlayer(rec, start) {
+    this.cancelTimer();
+    let st = start || rec.interval[0];
+    this.player.currentTime = st;
+    this.player.play();
+    this.ref = rec.ref;
+    this.onplay(rec.ref);
+    this.timer = setTimeout(
+      this.onTimeout,
+      1000 * (rec.interval[1] - st),
+      this,
+    );
+  }
+
+  pausePlayer() {
+    this.cancelTimer();
+    this.playPause = this.player.currentTime;
     this.player.pause();
+    this.onpause(this.ref);
+  }
+
+  playInterval(idx) {
+    this.playall = false;
+    const rec = this.timeTable[idx];
+    if (this.ref === rec.ref) {
+      if (this.player.paused) {
+        this.playPlayer(rec, this.playPause);
+      } else {
+        this.pausePlayer();
+      }
+    } else {
+      this.playPlayer(rec);
+    }
+  }
+
+  setPlayAll(val, idx) {
+    this.playall = val;
+    if (val) {
+      const rec = this.timeTable[idx];
+      if (this.ref === rec.ref) {
+        this.playPlayer(rec, this.playPause);
+      } else {
+        this.playPlayer(rec);
+      }
+    }
+  }
+
+  pause() {
+    this.pausePlayer();
   }
 }
